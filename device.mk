55fc4026ddc0cd41ea3926d89cfbc1d156481937
lahaina <nattolecats@gmail.com>
2025-02-09 00:59:25 +0900

    denniz: Remove VNDK conflicts * These are already commonized in mt6893-common

--- device.mk
+++ device.mk
@@ -37,10 +37,5 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayDenniz \
     WifiOverlayDenniz
 
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinder-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
-
 # Inherit from vendor blobs
 $(call inherit-product, vendor/oplus/denniz/denniz-vendor.mk)
